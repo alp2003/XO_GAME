50ccaf587c13191626ce32ba6d2aa0e0f4152f0b
alp2003 <alp2003@gmail.com>
2021-04-10 20:26:05 +0300

    Added Solution

---
+++ XOGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XOGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int boardSize = 3;
+
+            Console.WriteLine("XO GAME");
+            Console.WriteLine("===========");
+            Console.Write("Please enter board size:");
+            boardSize = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            XOBoard XOGame = new XOBoard(boardSize);
+            XOGame.Display();
+
+            Console.Write("");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ XOGame/XOBoard.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XOGame
+{
+    public class XOBoard
+    {
+        private int m_BoardLength;
+        private char[,] m_XOBoard;
+
+        public int BoardLength
+        {
+            get { return m_BoardLength; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    m_BoardLength = value;
+                }
+            }
+        }
+
+        public char[,] Board
+        {
+            get { return m_XOBoard; }
+        }
+
+        public XOBoard(int i_BoardSize)
+        {
+            BoardLength = i_BoardSize;
+            m_XOBoard = new char[BoardLength, BoardLength];
+        }
+
+        public void Put(string location)
+        {
+
+        }
+
+        public string Status()
+        {
+            return string.Empty;
+        }
+
+        public void Display()
+        {
+
+
+            Console.WriteLine("XO Game");
+            Console.WriteLine("==============");
+            char currentCharToPrint = 'A';
+            StringBuilder borderLine = new StringBuilder("  =");
+            StringBuilder currentLine = new StringBuilder(string.Empty);
+
+            currentLine.Append(" ");
+            for (int i = 0; i < BoardLength; i++)
+            {
+                currentLine.Append("   ").Append(currentCharToPrint++);
+                borderLine.Append("====");
+            }
+
+            Console.WriteLine(currentLine);
+            Console.WriteLine(borderLine);
+
+            for (int i = 1; i <= BoardLength; i++)
+            {
+                currentLine = new StringBuilder(string.Empty);
+                currentLine.Append(string.Format("{0}", i)).Append(" |");
+                for (int j = 1; j <= BoardLength; j++)
+                {
+                    currentLine.Append(' ').Append(Board[i - 1, j - 1]).Append(" |");
+                }
+
+                Console.WriteLine(currentLine);
+                Console.WriteLine(borderLine);
+            }
+
+        }
+    }
+}
